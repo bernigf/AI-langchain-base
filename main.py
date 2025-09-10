438af204d8917df3c789445ac03f5ab3ffc94d96
Bernardo Garcia Fuentes <bernigf@gmail.com>
2025-09-09 22:19:22 -0300

    Add OpenAI API key handling and initialize ChatOpenAI model

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import langchain
+import os
+
+from getpass import getpass
+from langchain_openai import ChatOpenAI
+
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY") or getpass("Enter OpenAI API Key: ")
 
 def main():
 
-    print("Hello langchain")
+    model = "gpt-5-mini"
+    print(f"Init langchain model '{model}..")
 
+    llm = ChatOpenAI(temperature=0.0, model=model)
+    
 main()
